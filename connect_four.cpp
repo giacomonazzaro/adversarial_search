d3c62b4094bc7df80641c041ca10e43ec12d1e53
giacomonazzaro <nazzarogiacomo@gmail.com>
2018-06-15 10:05:40 +0200

    looks like it's working

--- connect_four.cpp
+++ connect_four.cpp
@@ -7,6 +7,7 @@ o  o  o  o  o  o  o
 o  o  o  o  o  o  o
 */
 #include <vector>
+#include <cassert>
 #define array std::vector
 using GridConnect4 = array<int>;
 
@@ -59,16 +60,55 @@ bool check_diagonal_pos(const GridConnect4& grid, int index) {
     int player = grid[index];
     for(int i : {1,2,3}){
         int k = index + 7*i + i;
+        if(k%7 == 0) return false;
         if(k > 6*7) return false;
         if(grid[k] != player) return false;
     }
     return true;
 }
 
+bool check_diagonal_pos(const GridConnect4& grid) {
+    for (int col = 0; col < 3; ++col) {
+        int val = grid[col];
+        for(int row = 0; row < 6; row++) {
+            int count = 1;
+            if(val != grid[col + row*(7+1)]) {
+                val = grid[col + row*(7+1)];
+                count = 1;
+            }
+            else {
+                count += 1;
+            }
+            if(count == 4) return val;
+        }
+    }
+    return 0;
+}
+
+bool check_diagonal_neg(const GridConnect4& grid) {
+    for (int col = 3; col < 7; ++col) {
+        int val = grid[col];
+        int count = 1;
+        for(int row = 1; row < 6; row++) {
+            if(val != grid[col + row*(7-1)]) {
+                val = grid[col + row*(7-1)];
+                count = 1;
+            }
+            else {
+                count += 1;
+            }
+            if(count == 4) return val;
+        }
+    }
+    return 0;
+}
+
+
 bool check_diagonal_neg(const GridConnect4& grid, int index) {
     int player = grid[index];
     for(int i : {1,2,3}){
         int k = index + 7*i - i;
+        if((k+1)%7 == 0) return false;
         if(k > 6*7) return false;
         if(grid[k] != player) return false;
     }
@@ -76,75 +116,118 @@ bool check_diagonal_neg(const GridConnect4& grid, int index) {
 }
 
 int get_utility(const GridConnect4& grid) {
-    for (int i = 0; i < 6; ++i) {
-        for (int k = 0; k < 7; ++k) {
-            int index = i*7 + k;
+    for (int index = 0; index < 6*7; ++index) {
+            int row = index / 7;
+            int col = index % 7;
             if(grid[index] == 0) continue;
-            if(check_vertical(grid, index)) return grid[index];
-            if(check_horizontal(grid, index)) return grid[index];
-            if(check_diagonal_pos(grid, index)) return grid[index];
-            if(check_diagonal_neg(grid, index)) return grid[index];
-        }
+            if(row <= 2)
+                if(check_vertical(grid, index)) return grid[index];
+            if(col <= 3)
+                if(check_horizontal(grid, index)) return grid[index];
+            if(row <= 2 and col <=3)
+                if(check_diagonal_pos(grid, index)) return grid[index];
+            if(row <= 3 and col >= 3)
+                if(check_diagonal_neg(grid, index)) return grid[index];
     }
     return 0;
 }
 
-array<GridConnect4> make_children(const GridConnect4& grid, int player) {
-    array<GridConnect4> result;
+array<std::pair<GridConnect4, int>> make_children(const GridConnect4& grid, int player) {
+    array<std::pair<GridConnect4, int>> result;
     for (int i = 0; i < 7; ++i) {
         if(grid[i] != 0) continue;
         GridConnect4 g = grid;
         insert(g, i, player);
-        result.push_back(g);
+        result.push_back({g, i});
     }
 
     return result;
 }
 
-int best_move;
 
 template <typename State>
-int minimax(const State& state, bool max, int depth) {
+int minimax(const State& state, bool max, int depth, int& best_move) {
     // if(is_terminal(state)) return get_utility(state);
     int u = get_utility(state);
-    if(u != 0) return u;
+    if(u != 0) return u * (depth + 1);
     if(depth == 0) return 0;
 
     auto children = make_children(state, max? 1 : -1);
-    int value = minimax(children[0], not max, depth-1);
-    best_move = 0;
-    for (int i = 1; i < children.size(); ++i)
-    {
-        const auto& child = children[i];
-        int new_val = minimax(child, not max, depth-1);
+    int dummy;
+    int value = minimax(children[0].first, not max, depth-1, dummy);
+    best_move = children[0].second;
+    for (int i = 1; i < children.size(); ++i) {
+        const auto& child = children[i].first;
+        int new_val = minimax(child, not max, depth-1, dummy);
         if(max and new_val > value) {
             value = new_val;
-            best_move = i;
+            best_move = children[i].second;
         }
         if(not max and new_val < value) {
             value = new_val;
-            best_move = i;
+            best_move = children[i].second;
+        }
+    }
+
+    return value;
+}
+
+template <typename State>
+int alpha_beta_search(const State& state, bool max, int alpha, int beta, int depth, int& best_move) {
+    int u = get_utility(state);
+    if(u != 0) return u * (depth + 1);
+    if(depth == 0) return 0;
+
+    auto children = make_children(state, max? 1 : -1);
+    if(children.size() == 0) return 0;
+    
+    int dummy;
+    int value = alpha_beta_search(children[0].first, not max, alpha, beta, depth-1, dummy);
+    best_move = children[0].second;
+    for (int i = 1; i < children.size(); ++i) {
+        const auto& child = children[i].first;
+        int new_value = alpha_beta_search(child, not max, alpha, beta, depth-1, dummy);
+        if(max) {
+            if(new_value > value) {
+                value = new_value;
+                best_move = children[i].second;
+            }
+            if(value >= beta) {
+                return value;
+            }
+            alpha = std::max(alpha, value);
+        }
+        if(not max) {
+            if(new_value < value) {
+                value = new_value;
+                best_move = children[i].second;
+            }
+            if(value <= alpha) {
+                return value;
+            }
+            beta = std::min(beta, value);
         }
     }
     
     return value;
 }
 
+template <typename State>
+int alpha_beta_search(const State& state, bool max, int depth, int& best_move) {
+    return alpha_beta_search(state, max, -99999, 999999, depth, best_move);
+}
 
-void start_match() {
+void one_vs_one() {
     GridConnect4 grid (6*7, 0);
-    insert(grid, 2, 1);
-    insert(grid, 3, 1);
-
-    int col;
+    int move;
     int player = -1;
     print_grid(grid);
     while(true){
         printf("  next move: ");
-        scanf("%d", &col);
+        scanf("%d", &move);
         printf("\n");
-        if(col < 1 or col > 7) continue;
-        insert(grid, col-1, player);
+        if(move < 1 or move > 7) continue;
+        insert(grid, move-1, player);
         player = -player;
         print_grid(grid);
 
@@ -155,51 +238,55 @@ void start_match() {
     }
 }
 
-void ai_move(GridConnect4& grid, int color) {
-    minimax<GridConnect4>(grid, color>0, 7);
-    insert(grid, best_move, color);
+void ai_move(GridConnect4& grid, int color, int depth) {
+    int move;
+    // int value = minimax<GridConnect4>(grid, color>0, 8, move);
+    auto value = alpha_beta_search<GridConnect4>(grid, color>0, depth, move);
+    insert(grid, move, color);
+    printf("(%d) ", depth);
+    printf("move: %d, value: %d\n", move+1, value);
+    if(value * color > 0) printf("I won!!!\n");
+    if(value * color < 0) printf("I lost!!!\n");
 }
+
+bool is_full(const GridConnect4& grid) {
+    for(int i=0; i<7; i++)
+        if(grid[i] == 0) return false;
+
+    return true;
+}
+
 
 void beat_ai() {
     GridConnect4 grid (6*7, 0);
-    insert(grid, 2, -1);
-    insert(grid, 3, -1);
-    // insert(grid, 4, -1);
-
-    ai_move(grid, 1);
-    print_grid(grid);
-
-    // insert(grid, 2, -1);
-    // insert(grid, 3, 1);
-
-    // for(auto g : make_children(grid, -1)) {
-    //     print_grid(g);
-    //     printf("%d\n\n", get_utility(g));
-    // }
-
-    return;
-
-
-
-    int col;
     int player = 1;
+    int human_player = 1;
+    int ai_player = -human_player;
+    insert(grid, 3, ai_player);
+
     print_grid(grid);
-    while(true){
-        if(player == -1)
-            ai_move(grid, player);
+    while(true) {
+        if(player == ai_player)
+            ai_move(grid, ai_player, 11);
         else {
-            printf("  next move: ");
-            scanf("%d", &col);
-            printf("\n");   
-            printf("\n");
-            if(col < 1 or col > 7) continue;
-            insert(grid, col-1, player);
+            ai_move(grid, -ai_player, 7);
+
+            // printf("  next move: ");
+            // scanf("%d", &col);
+            // if(col < 1 or col > 7) continue;
+            // insert(grid, col-1, human_player);
         }
+        printf("\n");   
+        printf("\n");
 
         player = -player;
         print_grid(grid);
         if(get_utility(grid) != 0) {
             printf("\nPlayer %d wins!\n\n", get_utility(grid));
+            return;
+        }
+        if(is_full(grid)) {
+            printf("\nTie!\n\n");
             return;
         }
     }
@@ -208,12 +295,6 @@ void beat_ai() {
 int main(int argc, char const *argv[])
 {
     beat_ai();
-    // start_match();
-    // GridConnect4 grid (6*7, 0);
-
-    // int v = minimax<GridConnect4>(grid, true, 6);
-    // printf("%d\n", v);
-    // for(auto i : moves) printf("%d ", i);
-    // printf("\n");
+    // one_vs_one();
     return 0;
 }
